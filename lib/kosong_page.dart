2f47232e5552e219172cb89e781f29dccc9ce9bf
TissuGalon <parzivalxdd@gmail.com>
2026-05-05 12:13:23 +0700

    Setup Requirement

---
+++ lib/kosong_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:pos_mobile/Configuration/components.dart';
+import 'package:pos_mobile/configuration/configuration.dart';
+
+class KosongPage extends StatelessWidget {
+  const KosongPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Warna.bg,
+      appBar: MyAppBar(title: 'Empty Page'),
+      body: Center(child: Column()),
+    );
+  }
+}
